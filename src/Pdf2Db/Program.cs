51a5489102f58e645b680a6655a016957a820f59
Volkan Şendağ <vsendag@gmail.com>
2022-08-12 10:53:50 +0300

    init proj

---
+++ src/Pdf2Db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pdf2Db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
